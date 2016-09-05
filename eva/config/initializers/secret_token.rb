f73447bc1b72f37544fbc77c2346ab33f0dba95e
evangelinaf <efiorucci@gmail.com>
2016-09-05 15:12:23 -0300

    Adding ruby env

---
+++ eva/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eva::Application.config.secret_key_base = 'b836b4bf18e38abb66ed52999bda4fb4c356eefcc70fd3ea0f3ddd0ca9788a5369a53273a677b11c2ba5d79521ad89c48ef9d1b6ee1a401c0a79162642bb0c6a'
---
+++ eva/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Eva::Application.config.session_store :cookie_store, key: '_eva_session'
